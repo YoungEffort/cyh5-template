817983183d5644e1085e97e5035ef0aff265f74b
youngeffort <893520964@qq.com>
2019-08-08 11:45:44 +0800

    feat：init

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+/*
+* 组件统一输出
+*  秦国胜
+* 2019-07-10
+* */
+
+import globalToast from './global-toast/globalToast' // 全局toast
+import myInput from './my-input/my-input' // 表单定制
+import mySearch from './my-search/my-search' // 表单定制
+export {
+  globalToast,
+  myInput,
+  mySearch
+}
---
+++ src/views/login/login.vue
@@ -0,0 +1,20 @@
+<!--
+  登录
+  秦国胜
+  2019-07-10
+-->
+<template>
+    <div>
+      登录
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
